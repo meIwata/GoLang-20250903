d0fbc3e3e1b772958aadfc50b950c09ee4830503
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2025-09-03 13:14:22 +0800

    重構投資計算器邏輯並新增通膨調整功能

--- investment_calculator.go
+++ investment_calculator.go
@@ -9,13 +9,26 @@ func main() {
 	// fmt.Println("Hello, World!")
 	// fmt.Print(`Hello, World!`)
 
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+	// var investmentAmount float64 = 1000
+	// var investmentAmount, years float64 = 1000, 10
+	// investmentAmount, years := 1000.0, 10.0
 
-	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	const inflationRate float64 = 2.5
+	var investmentAmount float64 = 1000
+	var years float64 = 10
 
-	fmt.Println("未來價值:", futureValue)
+	// var expectedReturnRate float64 = 5.5
+	expectedReturnRate := 5.5 // 也可以寫成這樣
+
+	// var years float64 = 10
+
+	// var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	// var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, float64(years))
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	fmt.Println("未來預估價值:", futureValue)
+	fmt.Println("未來實際價值:", futureRealValue)
 	// 要執行的時候可以在 terminal 輸入 go run investment_calculator.go 或是 investment_calculator 或是 go run .
 }
 
